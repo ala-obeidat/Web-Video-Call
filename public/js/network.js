dfcb7b77fb27820da50f6e3630fecd0c1bc24d80
Ala Obeidat <ala-work@hotmail.com>
2021-04-10 02:27:37 +0300

    Add connection information

---
+++ public/js/network.js
@@ -0,0 +1,46 @@
+const intervalToCheckInSeconds = 3;
+const speedDisplay = document.getElementById("speed");
+const statusDisplay = document.getElementById("status");
+
+const checkSlowConnection =  (seconds) => {  
+    
+	let speed="Excellent";
+	if(seconds > 1 && seconds < 3)
+		speed= "Good";
+	if(seconds > 3 && seconds < 10)
+		speed= "Poor";
+	if(seconds > 10)
+		speed= "Slow"; 
+	if(seconds < 0)
+		speed= "No"; 
+	speedDisplay.textContent = speed + ' Connection';
+    speedDisplay.className = 'speed-'+speed;
+}
+
+const checkOnlineStatus = async () => {
+  try {
+  let countDownDate = new Date().getTime();
+    const online = await fetch("http://1x1px.me/FF4D00-0.8.png",{mode: "no-cors"});
+	let newDate = new Date().getTime();
+	 let distance = newDate - countDownDate;
+	 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	 checkSlowConnection(seconds);
+    return online.status ==0;
+  } catch (err) {
+  checkSlowConnection(-1);
+    return false; // definitely offline
+  }
+};
+const SetStatusDisplayContet = async () => {
+let result= await checkOnlineStatus() ? "Online" : "OFFline";
+statusDisplay.textContent = result;
+statusDisplay.className = 'result-'+result;
+}
+setInterval(async () => { 
+  await SetStatusDisplayContet();
+}, intervalToCheckInSeconds * 1000); 
+
+// forgot to include async load event listener in the video! 
+window.addEventListener("load", async (event) => { 
+  await SetStatusDisplayContet();
+});
